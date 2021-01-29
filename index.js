681158e565f66514a95a53f6b8672ed9eaf04465
Kushagra Agarwal <kushagracool490@gmail.com>
2021-01-29 18:31:32 +0530

    Done

---
+++ index.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer((req,res) => {
+    if(req.url === '/') {
+        fs.readFile('index.html',(err,data) => {
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        })
+    } else if(req.url === '/about') {
+        fs.readFile('about.html',(err,data) => {
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        })
+    } else if(req.url === '/contact-me') {
+        fs.readFile('contact-me.html',(err,data) => {
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        })
+    } else {
+        fs.readFile('404.html',(err,data) => {
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        })
+    }
+}).listen(3000);
